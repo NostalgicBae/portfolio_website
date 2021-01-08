e2aa713e2eccd35d29123629994ea553c780dc27
Nostalgicbae <nostalgicbae@MSI.localdomain>
2021-01-08 01:38:13 +0100

    started JS

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById("portfolio-button-1").onclick = function () {
+    window.open("https://nostalgicbae.github.io/company_home_page/");
+};
+
+document.getElementById("portfolio-button-2").onclick = function () {
+    window.open("https://nostalgicbae.github.io/Etch-a-Sketch/");
+};
+
+document.getElementById("portfolio-button-3").onclick = function () {
+    window.open("https://nostalgicbae.github.io/calculator/");
+};
+
+document.getElementById("portfolio-button-4").onclick = function () {
+    window.open("https://nostalgicbae.github.io/google-homepage/");
+};
+
